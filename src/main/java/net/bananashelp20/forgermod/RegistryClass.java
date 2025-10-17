cc4691fa19486e0658bd7a8a6784916890870743
William Riegler <bananashelp20@gmail.com>
2025-10-17 17:17:02 +0200

    first commit here

---
+++ src/main/java/net/bananashelp20/forgermod/RegistryClass.java
@@ -0,0 +1,9 @@
+package net.bananashelp20.forgermod;
+
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+
+public class RegistryClass {
+    public static void acceptItemsToForgerIngredientsTab(BuildCreativeModeTabContentsEvent event) {
+
+    }
+}
