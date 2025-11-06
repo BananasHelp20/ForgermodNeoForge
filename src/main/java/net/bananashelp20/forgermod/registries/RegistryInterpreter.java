78ed17cf41d3911ed696ebb70d42e615fdfcd8c7
20230036 <20230036@students.htl-perg.ac.at>
2025-11-06 19:12:11 +0100

    hüfe, ds geht nd

--- src/main/java/net/bananashelp20/forgermod/registries/RegistryInterpreter.java
+++ src/main/java/net/bananashelp20/forgermod/registries/RegistryInterpreter.java
@@ -1,5 +1,7 @@
 package net.bananashelp20.forgermod.registries;
 
+import org.checkerframework.checker.units.qual.A;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -13,7 +15,6 @@ public class RegistryInterpreter {
     static File modBlocksFile = new File("./src/main/java/net/bananashelp20/forgermod/block/ModBlocks.java");
     static File itemFile = new File("./src/main/java/net/bananashelp20/forgermod/registries/regFiles/items.txt");
     static File modItemsFile = new File("./src/main/java/net/bananashelp20/forgermod/item/ModItems.java");
-    static File creativeTabFile = new File("./src/main/java/net/bananashelp20/forgermod/registries/regFiles/creativeTabs.txt");
     static File modCreativeModeTabsFile = new File("./src/main/java/net/bananashelp20/forgermod/CreativeModeTabs/ModCreativeModeTabs.java");
     static File modRegistry = new File("./src/main/java/net/bananashelp20/forgermod/registries/RegistryClass.java");
     static File testFile = new File("./src/main/java/net/bananashelp20/forgermod/registries/TestReg.java");
@@ -21,7 +22,6 @@ public class RegistryInterpreter {
     static File modBlockLootTableProvider = new File("./src/main/java/net/bananashelp20/forgermod/registries/TestRegistryClass.java");
     static File modBlockStateProvider = new File("./src/main/java/net/bananashelp20/forgermod/registries/TestRegistryClass.java");
     static File modBlockTagProvider = new File("./src/main/java/net/bananashelp20/forgermod/registries/TestRegistryClass.java");
-
 
     static String modBlocksFileContent = getWholeFileContentTillGenerate(modBlocksFile);
     static String modItemsFileContent = getWholeFileContentTillGenerate(modItemsFile);
@@ -42,12 +42,12 @@ public class RegistryInterpreter {
                 && modRegistry.exists() && modRegistry.canWrite() && modRegistry.canRead()
                 && blockFile.exists() && blockFile.canRead()
                 && itemFile.exists() && itemFile.canRead()
-                && creativeTabFile.exists() && creativeTabFile.canRead()
         )) {
             return false;
         }
         try {
             generateAndWriteBlocks(testFile, blockFile);
+            generateAndWriteBlockDatagen();
         } catch (Exception e) {
             System.err.println(e);
         }
@@ -89,29 +89,27 @@ public class RegistryInterpreter {
         String blockTagToolType = "";
         String method = "";
         String blockModel = "";
-        try {
-            for (int i = 0; reader2.hasNextLine() && i < index; i++) {
-                reader2.nextLine();
-            }
-            for (int i = 0; reader2.hasNextLine(); i++) {
-                name = reader2.nextLine().trim();
-                if (name.equals("}")) {
-                    return output;
-                }
-                method = reader2.nextLine().trim();
-                properties = reader2.nextLine().trim();
-                dropOtherMethod = reader2.nextLine().trim();
-                if (!dropOtherMethod.equals("dropSelf")) {
-                    dropOtherItem = reader2.nextLine().trim();
-                }
-                blockModel = reader2.nextLine().trim();
-                blockTagTool = reader2.nextLine().trim();
-                blockTagToolType = reader2.nextLine().trim();
-                output += "    public static final DeferredBlock<Block> " + name.toUpperCase() + " = " + method + "(\"" + name.toLowerCase() + "\", " + properties + ");\n";
-            }
-        } catch (Exception e) {
-            System.err.println("hüfe");;
+        for (int i = 0; reader2.hasNextLine() && i < index; i++) {
+            reader2.nextLine();
         }
+        for (int i = 0; reader2.hasNextLine(); i++) {
+            name = reader2.nextLine().trim();
+            if (name.equals("}")) {
+                return output;
+            }
+            method = reader2.nextLine().trim();
+            properties = reader2.nextLine().trim();
+            dropOtherMethod = reader2.nextLine().trim();
+            if (!dropOtherMethod.equals("dropSelf")) {
+                dropOtherItem = reader2.nextLine().trim();
+            }
+            blockModel = reader2.nextLine().trim();
+            blockTagTool = reader2.nextLine().trim();
+            blockTagToolType = reader2.nextLine().trim();
+            reader2.nextLine();
+            output += "    public static final DeferredBlock<Block> " + name.toUpperCase() + " = " + method + "(\"" + name.toLowerCase() + "\", " + properties + ");\n";
+        }
+        reader2.close();
         return output + "}";
     }
 
@@ -143,11 +141,13 @@ public class RegistryInterpreter {
                     blockModel = reader2.nextLine().trim();
                     blockTagTool = reader2.nextLine().trim();
                     blockTagToolType = reader2.nextLine().trim();
+                    reader2.nextLine().trim();
                     output += "    public static final DeferredBlock<Block> " + name.toUpperCase() + " = createSimpleBlock(\"" + name.toLowerCase() + "\", " + properties + ");\n";
                 }
             }
         } catch (Exception e) {
-            System.err.println("hüfe");;
+            System.err.println("hüfe");
+            throw e;
         }
 
         return output;
@@ -181,18 +181,21 @@ public class RegistryInterpreter {
                     blockModel = reader2.nextLine().trim();
                     blockTagTool = reader2.nextLine().trim();
                     blockTagToolType = reader2.nextLine().trim();
+                    reader2.nextLine().trim();
                     output += "    public static final DeferredBlock<Block> " + name.toUpperCase() + " = registerBlock(\"" + name.toLowerCase() + "\",\n" +
                             "            () -> " + properties +
                             ");\n";
+                    reader2.close();
                 }
             }
         } catch (Exception e) {
-            System.err.println("hüfe");
+            System.err.println("hüfe:" + e);
+            throw e;
         }
         return "";
     }
 
-    public static void datagenBlockInit() throws IOException {
+    public static void generateAndWriteBlockDatagen() throws IOException {
         String generatedBlockLootTables = getWholeFileContentTillGenerate(modBlockLootTableProvider);
         String generatedBlockStateProvider = getWholeFileContentTillGenerate(modBlockStateProvider);
         String generatedBlockTagProvider = getWholeFileContentTillGenerate(modBlockTagProvider);
@@ -203,14 +206,13 @@ public class RegistryInterpreter {
         FileWriter loottableWriter = new FileWriter(modBlockLootTableProvider.getPath());
         FileWriter stateWriter = new FileWriter(modBlockStateProvider.getPath());
         FileWriter tagWriter = new FileWriter(modBlockTagProvider.getPath());
-        ArrayList<ArrayList<String>> simpleData = new ArrayList<ArrayList<String>>();
-        ArrayList<ArrayList<String>> specialData = new ArrayList<ArrayList<String>>();
-        ArrayList<ArrayList<String>> complexData = new ArrayList<ArrayList<String>>();
+        ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
         String dropOtherMethod;
         String dropOtherItem = "";
         String blockTagTool;
         String blockTagToolType;
         String blockModel;
+        String creativeTab = "";
         String line = "";
         String name = "";
         for (int i = 0; reader.hasNextLine(); i++) {
@@ -226,13 +228,15 @@ public class RegistryInterpreter {
                     blockModel = reader.nextLine().trim();
                     blockTagTool = reader.nextLine().trim();
                     blockTagToolType = reader.nextLine().trim();
-                    simpleData.add(new ArrayList<String>());
-                    simpleData.get(j).add(name);
-                    simpleData.get(j).add(dropOtherMethod);
-                    if (!dropOtherItem.equals("")) simpleData.get(j).add(dropOtherItem);
-                    simpleData.get(j).add(blockModel);
-                    simpleData.get(j).add(blockTagTool);
-                    simpleData.get(j).add(blockTagToolType);
+                    creativeTab = reader.nextLine().trim();
+                    data.add(new ArrayList<String>());
+                    data.get(j).add(name);
+                    data.get(j).add(dropOtherMethod);
+                    if (!dropOtherItem.equals("")) data.get(j).add(dropOtherItem);
+                    data.get(j).add(blockModel);
+                    data.get(j).add(blockTagTool);
+                    data.get(j).add(blockTagToolType);
+                    data.get(j).add(creativeTab);
                 }
             } else if (line.equalsIgnoreCase("special {")) {
                 for (int j = 0; (name = reader.nextLine().trim()).equals("}"); j++) {
@@ -246,13 +250,15 @@ public class RegistryInterpreter {
                     blockModel = reader.nextLine().trim();
                     blockTagTool = reader.nextLine().trim();
                     blockTagToolType = reader.nextLine().trim();
-                    specialData.add(new ArrayList<String>());
-                    specialData.get(j).add(name);
-                    specialData.get(j).add(dropOtherMethod);
-                    if (!dropOtherItem.equals("")) specialData.get(j).add(dropOtherItem);
-                    specialData.get(j).add(blockModel);
-                    specialData.get(j).add(blockTagTool);
-                    specialData.get(j).add(blockTagToolType);
+                    creativeTab = reader.nextLine().trim();
+                    data.add(new ArrayList<String>());
+                    data.get(j).add(name);
+                    data.get(j).add(dropOtherMethod);
+                    if (!dropOtherItem.equals("")) data.get(j).add(dropOtherItem);
+                    data.get(j).add(blockModel);
+                    data.get(j).add(blockTagTool);
+                    data.get(j).add(blockTagToolType);
+                    data.get(j).add(creativeTab);
                 }
             } else if (line.equalsIgnoreCase("complex {")) {
                 for (int j = 0; (name = reader.nextLine().trim()).equals("}"); j++) {
@@ -265,16 +271,158 @@ public class RegistryInterpreter {
                     blockModel = reader.nextLine().trim();
                     blockTagTool = reader.nextLine().trim();
                     blockTagToolType = reader.nextLine().trim();
-                    complexData.add(new ArrayList<String>());
-                    complexData.get(j).add(name);
-                    complexData.get(j).add(dropOtherMethod);
-                    if (!dropOtherItem.equals("")) complexData.get(j).add(dropOtherItem);
-                    complexData.get(j).add(blockModel);
-                    complexData.get(j).add(blockTagTool);
-                    complexData.get(j).add(blockTagToolType);
+                    creativeTab = reader.nextLine().trim();
+                    data.add(new ArrayList<String>());
+                    data.get(j).add(name);
+                    data.get(j).add(dropOtherMethod);
+                    if (!dropOtherItem.equals("")) data.get(j).add(dropOtherItem);
+                    data.get(j).add(blockModel);
+                    data.get(j).add(blockTagTool);
+                    data.get(j).add(blockTagToolType);
+                    data.get(j).add(creativeTab);
+
                 }
             }
         }
+        reader.close();
+        generateDatagenBlockCode(data);
+    }
+
+    public static void  generateDatagenBlockCode(ArrayList<ArrayList<String>> data) throws IOException {
+        String generatedBlockLootTables = getWholeFileContentTillGenerate(modBlockLootTableProvider);
+        String generatedBlockStates = getWholeFileContentTillGenerate(modBlockStateProvider);
+        String generatedBlockTags = getWholeFileContentTillGenerate(modBlockTagProvider);
+        int writableLoottablePos = getWritablePos(modBlockLootTableProvider, "//generate DROPS!");
+        int writableStatePos = getWritablePos(modBlockLootTableProvider, "//generate MODELS!");
+        int writableTagPos = getWritablePos(modBlockLootTableProvider, "//generate TAGS!");
+
+        int normalListSize = (data.get(0).get(2).equals("dropSelf") ? data.get(0).size() : data.get(0).size()-1);
+
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).get(0).equals("dropSelf") || data.get(i).get(0).equals("dropWhenSilkTouch")) {
+                generatedBlockLootTables += "        " + data.get(i).get(2) + "(ModBlocks." + data.get(i).get(0).toUpperCase() + ".get());\n";
+            }
+            if (data.get(i).get(0).equals("dropOther")) {
+                generatedBlockLootTables += "        dropOther(ModBlocks." + data.get(i).get(0).toUpperCase() + ".get(), " + data.get(i).get(3).toUpperCase() + ".get());\n";
+            }
+        }
+        generatedBlockLootTables += "    }\n}";
+
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).get(3).equals("blockWithItem") || data.get(i).get(4).equals("blockWithItem")) {
+                generatedBlockStates += "blockWithItem(ModBlocks." + data.get(i).get(0) +");\n";
+            }
+        }
+        generatedBlockStates+= "    }\n}";
+
+        String pickaxeTags = "";
+        String axeTags = "";
+        String shovelTags = "";
+        String hoeTags = "";
+
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).get(4).equals("pickaxe") || data.get(i).get(5).equals("pickaxe")) {
+                pickaxeTags += "                .add(ModBlocks." + data.get(i).get(0).toUpperCase() + ".get())\n";
+            } else if (data.get(i).get(4).equals("axe") || data.get(i).get(5).equals("axe")) {
+                axeTags += "                .add(ModBlocks." + data.get(i).get(0).toUpperCase() + ".get())\n";
+            } else if (data.get(i).get(4).equals("shovel") || data.get(i).get(5).equals("shovel")) {
+                shovelTags += "                .add(ModBlocks." + data.get(i).get(0).toUpperCase() + ".get())\n";
+            } else if (data.get(i).get(4).equals("hoe") || data.get(i).get(5).equals("hoe")) {
+                hoeTags += "                .add(ModBlocks." + data.get(i).get(0).toUpperCase() + ".get())\n";
+            }
+        }
+        generatedBlockTags += generateToolTags(new String[] {pickaxeTags, axeTags, shovelTags, hoeTags});
+        ArrayList<String> typeTags = new ArrayList<>();
+        ArrayList<String> types = new ArrayList<>();
+        ArrayList<ArrayList<String>> tagsByType = new ArrayList<ArrayList<String>>();
+        Scanner tagReader = new Scanner(modBlockTagProvider);
+        String current;
+        for (int i = 0; tagReader.hasNextLine(); i++) {
+            current = tagReader.nextLine().trim();
+            if (current.contains("tag(") && current.contains("NEEDS_")) {
+                typeTags.add(current);
+            }
+        }
+        tagReader.close();
+
+        for (int i = 0; i < typeTags.size(); i++) {
+            types.add(typeTags.get(i).substring(25, typeTags.get(i).length() - 6).toLowerCase());
+        }
+        Scanner tagReader2 = new Scanner(blockFile);
+        String tags = "";
+        String line = "";
+        String name = "";
+        String drops = "";
+        String currentTag = "";
+        for (int i = 0; i < types.size(); i++) {
+            tagsByType.get(i).set(0, types.get(i));
+        }
+
+        for (int j = 0; tagReader2.hasNextLine() && !name.equals("}"); j++) {
+            line = tagReader2.nextLine().trim();
+            if (line.equalsIgnoreCase("simple {") || line.equalsIgnoreCase("special {") || line.equalsIgnoreCase("complex {")) {
+                tags += "\n    //" + line.substring(0, 6)+ " blocks\n";
+                name = tagReader2.nextLine().trim();
+                tagReader2.nextLine();
+                drops = tagReader2.nextLine().trim();
+                tagReader2.nextLine();
+                if (drops.equals("dropSelf") || drops.equals("dropWhenSilkTouch")) {
+                    currentTag = tagReader2.nextLine().trim();
+                    tagsByType.get(getIndexByValue(currentTag, tagsByType)).set(j, name);
+                } else {
+                    tagReader2.nextLine();
+                    currentTag = tagReader2.nextLine().trim();
+                    tagsByType.get(getIndexByValue(currentTag, tagsByType)).set(j, name);
+                }
+            }
+        }
+        generatedBlockTags += generateToolTypeTags(typeTags, tagsByType);
+        generatedBlockTags += "    }\n}";
+
+        FileWriter loottableWriter = new FileWriter(modBlockLootTableProvider.getPath());
+        FileWriter stateWriter = new FileWriter(modBlockStateProvider.getPath());
+        FileWriter tagWriter = new FileWriter(modBlockTagProvider.getPath());
+
+        loottableWriter.write(generatedBlockLootTables);
+        stateWriter.write(generatedBlockStates);
+        tagWriter.write(generatedBlockTags);
+
+        loottableWriter.close();
+        stateWriter.close();
+        tagWriter.close();
+    }
+
+    public static int getIndexByValue(String searched, ArrayList<ArrayList<String>> tags) {
+        for (int i = 0; i < tags.size(); i++) {
+            if (tags.get(i).get(0).equals(searched)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static String generateToolTypeTags(ArrayList<String> types, ArrayList<ArrayList<String>> tagsByType) {
+        String generated = "";
+        for (int i = 0; i < types.size(); i++) {
+            generated += "        " + types.get(i);
+            for (int j = 0; j < tagsByType.size(); j++) {
+                if (types.get(i).substring(26, types.get(i).length()-6).equals(tagsByType.get(j).get(0))) {
+                    for (int k = 0; k < tagsByType.get(j).size(); k++) {
+                        generated += "                .add(ModBlocks." + tagsByType.get(j).get(k) + ".get())\n";
+                    }
+                }
+            }
+            generated += "        ;\n";
+        }
+        return generated;
+    }
+
+    public static String generateToolTags(String[] tags) {
+        return "        tag(BlockTags.MINEABLE_WITH_PICKAXE)\n" +
+                tags[0] + "        ;\n        tag(BlockTags.MINEABLE_WITH_AXE)\n" +
+                tags[1] + "        ;\n        tag(BlockTags.MINEABLE_WITH_SHOVEL)\n" +
+                tags[2] + "        ;\n        tag(BlockTags.MINEABLE_WITH_HOE)\n" +
+                tags[3] + "        ;\n\n";
     }
 
     public static String generateCreativeModeTabs(File fileToWrite, File registryFile) {
